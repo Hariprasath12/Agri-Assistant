0c4cf0a9934487039321b0312f58098d9198e952
hariprasath <hariprasath.1295@gmail.com>
2017-02-28 00:47:21 +0530

    update

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser')
-const cors=require('cors');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const config=require('./config/database');
-const app = express();
-const users=require('./routes/users');
+const config = require('./config/database');
 
 // Connect To Database
 mongoose.connect(config.database);
-console.log(config.database);
+
 // On Connection
-mongoose.connection.once('open', function() {
-  console.log("connected");
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
 });
 
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Port Number
 const port = 3000;
+
+// CORS Middleware
 app.use(cors());
-// static folder
-app.use(express.static('public'))
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
 app.use(bodyParser.json());
-app.use('/users',users);
 
-app.get('/',(req,res)=>{
-	res.send("hello");
-})
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+// Start Server
 app.listen(port, () => {
-    console.log("Server started on port" + port);
+  console.log('Server started on port '+port);
 });
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports={
-	database:"mongodb://localhost:27017/agri"
-
+module.exports = {
+  database: 'mongodb://localhost:27017/agri',
+  secret: 'yoursecret'
 }
